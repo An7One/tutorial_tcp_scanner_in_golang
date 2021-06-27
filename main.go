8a2e2465df66728dbc6001db99ae084cd7ebee4e
Leon <yanglyu.leon.7@gmail.com>
2021-06-26 23:52:52 -0700

    Asynchronous TCP Scanner in Goroutine Pool

--- main.go
+++ main.go
@@ -3,28 +3,58 @@ package main
 import (
 	"fmt"
 	"net"
-	"sync"
-	"time"
+	"sort"
 )
 
-func main() {
-	start := time.Now()
-	var wg sync.WaitGroup
-	for i := 21; i < 120; i++ {
-		wg.Add(1)
-		go func(j int) {
-			defer wg.Done()
-			address := fmt.Sprintf("20.194.168.28:%d", j)
-			conn, err := net.Dial("tcp", address)
-			if err != nil {
-				fmt.Printf("%s is closed\n", address)
-				return
-			}
-			conn.Close()
-			fmt.Printf("%s is open\n", address)
-		}(i)
+func worker(ports chan int, results chan int) {
+	for p := range ports {
+		address := fmt.Sprintf("20.194.168.9:%d", p)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			results <- 0
+			continue
+		}
+		conn.Close()
+		results <- p
 	}
-	wg.Wait()
-	elapse := time.Since(start)
-	fmt.Printf("\n\n%d seconds", elapse)
+}
+
+func main() {
+	ports := make(chan int, 100)
+	results := make(chan int)
+	var openPorts []int
+	var closedPorts []int
+
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
+	}
+
+	go func() {
+		for i := 0; i < 1024; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i < 1024; i++ {
+		port := <-results
+		if port != 0 {
+			openPorts = append(openPorts, port)
+		} else {
+			closedPorts = append(closedPorts, port)
+		}
+	}
+
+	close(ports)
+	close(results)
+
+	// all closed ports are 0
+	// sort.Ints(closedPorts)
+	// for _, port := range closedPorts {
+	// 	fmt.Printf("%d is closed\n", port)
+	// }
+
+	sort.Ints(openPorts)
+	for _, port := range openPorts {
+		fmt.Printf("%d is open\n", port)
+	}
 }
